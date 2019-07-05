7c58e277ccbb1b209946aa242f24870a83e97673
lhywell <lihuiyin128@163.com>
2019-07-05 15:41:30 +0800

    更新

---
+++ example4/config.js
@@ -0,0 +1,726 @@
+const BAIDU_MAP_STYLE = [{
+    "featureType": "highway",
+    "elementType": "geometry.fill",
+    "stylers": {
+        "color": "#ffdfacff"
+    }
+}, {
+    "featureType": "highway",
+    "elementType": "geometry.stroke",
+    "stylers": {
+        "color": "#e2a135ff"
+    }
+}, {
+    "featureType": "railway",
+    "elementType": "geometry.fill",
+    "stylers": {
+        "color": "#babec4ff"
+    }
+}, {
+    "featureType": "subway",
+    "elementType": "geometry",
+    "stylers": {
+        "visibility": "on",
+        "weight": 2.7
+    }
+}, {
+    "featureType": "district",
+    "elementType": "labels.text.fill",
+    "stylers": {
+        "color": "#5e5e5eff"
+    }
+}, {
+    "featureType": "nationalway",
+    "elementType": "labels.text.fill",
+    "stylers": {
+        "color": "#696969ff"
+    }
+}, {
+    "featureType": "subwaylabel",
+    "elementType": "labels.text.fill",
+    "stylers": {
+        "color": "#6d6d6dff"
+    }
+}, {
+    "featureType": "subwaylabel",
+    "elementType": "labels.text.stroke",
+    "stylers": {
+        "color": "#ffffffff",
+        "weight": 2
+    }
+}, {
+    "featureType": "provincialway",
+    "elementType": "labels.text.fill",
+    "stylers": {
+        "color": "#858585ff"
+    }
+}, {
+    "featureType": "cityhighway",
+    "elementType": "labels.text.fill",
+    "stylers": {
+        "color": "#616161ff"
+    }
+}, {
+    "featureType": "poilabel",
+    "elementType": "labels.icon",
+    "stylers": {
+        "visibility": "on"
+    }
+}, {
+    "featureType": "educationlabel",
+    "elementType": "labels.text",
+    "stylers": {
+        "fontsize": 22
+    }
+}, {
+    "featureType": "scenicspotslabel",
+    "elementType": "labels.text",
+    "stylers": {
+        "fontsize": 22
+    }
+}, {
+    "featureType": "scenicspotslabel",
+    "elementType": "labels.icon",
+    "stylers": {
+        "visibility": "off"
+    }
+}, {
+    "featureType": "airportlabel",
+    "elementType": "labels.text",
+    "stylers": {
+        "fontsize": 22
+    }
+}, {
+    "featureType": "medicallabel",
+    "elementType": "labels.text",
+    "stylers": {
+        "fontsize": 22
+    }
+}, {
+    "featureType": "educationlabel",
+    "stylers": {
+        "curZoomRegionId": "0",
+        "curZoomRegion": "11,14",
+        "level": "11"
+    }
+}, {
+    "featureType": "educationlabel",
+    "stylers": {
+        "curZoomRegionId": "0",
+        "curZoomRegion": "11,14",
+        "level": "12"
+    }
+}, {
+    "featureType": "educationlabel",
+    "stylers": {
+        "curZoomRegionId": "0",
+        "curZoomRegion": "11,14",
+        "level": "13"
+    }
+}, {
+    "featureType": "educationlabel",
+    "stylers": {
+        "curZoomRegionId": "0",
+        "curZoomRegion": "11,14",
+        "level": "14"
+    }
+}, {
+    "featureType": "educationlabel",
+    "elementType": "labels",
+    "stylers": {
+        "visibility": "off",
+        "curZoomRegionId": "0",
+        "curZoomRegion": "11,14",
+        "level": "11"
+    }
+}, {
+    "featureType": "educationlabel",
+    "elementType": "labels",
+    "stylers": {
+        "visibility": "off",
+        "curZoomRegionId": "0",
+        "curZoomRegion": "11,14",
+        "level": "12"
+    }
+}, {
+    "featureType": "educationlabel",
+    "elementType": "labels",
+    "stylers": {
+        "visibility": "off",
+        "curZoomRegionId": "0",
+        "curZoomRegion": "11,14",
+        "level": "13"
+    }
+}, {
+    "featureType": "educationlabel",
+    "elementType": "labels",
+    "stylers": {
+        "visibility": "off",
+        "curZoomRegionId": "0",
+        "curZoomRegion": "11,14",
+        "level": "14"
+    }
+}, {
+    "featureType": "businesstowerlabel",
+    "elementType": "labels.text",
+    "stylers": {
+        "fontsize": 22
+    }
+}, {
+    "featureType": "restaurantlabel",
+    "elementType": "labels.text",
+    "stylers": {
+        "fontsize": 22
+    }
+}, {
+    "featureType": "restaurantlabel",
+    "stylers": {
+        "curZoomRegionId": "0",
+        "curZoomRegion": "13,16",
+        "level": "13"
+    }
+}, {
+    "featureType": "restaurantlabel",
+    "stylers": {
+        "curZoomRegionId": "0",
+        "curZoomRegion": "13,16",
+        "level": "14"
+    }
+}, {
+    "featureType": "restaurantlabel",
+    "stylers": {
+        "curZoomRegionId": "0",
+        "curZoomRegion": "13,16",
+        "level": "15"
+    }
+}, {
+    "featureType": "restaurantlabel",
+    "stylers": {
+        "curZoomRegionId": "0",
+        "curZoomRegion": "13,16",
+        "level": "16"
+    }
+}, {
+    "featureType": "restaurantlabel",
+    "elementType": "labels",
+    "stylers": {
+        "visibility": "off",
+        "curZoomRegionId": "0",
+        "curZoomRegion": "13,16",
+        "level": "13"
+    }
+}, {
+    "featureType": "restaurantlabel",
+    "elementType": "labels",
+    "stylers": {
+        "visibility": "off",
+        "curZoomRegionId": "0",
+        "curZoomRegion": "13,16",
+        "level": "14"
+    }
+}, {
+    "featureType": "restaurantlabel",
+    "elementType": "labels",
+    "stylers": {
+        "visibility": "off",
+        "curZoomRegionId": "0",
+        "curZoomRegion": "13,16",
+        "level": "15"
+    }
+}, {
+    "featureType": "restaurantlabel",
+    "elementType": "labels",
+    "stylers": {
+        "visibility": "off",
+        "curZoomRegionId": "0",
+        "curZoomRegion": "13,16",
+        "level": "16"
+    }
+}, {
+    "featureType": "restaurantlabel",
+    "elementType": "labels.icon",
+    "stylers": {
+        "visibility": "off",
+        "curZoomRegionId": "0",
+        "curZoomRegion": "13,16",
+        "level": "13"
+    }
+}, {
+    "featureType": "restaurantlabel",
+    "elementType": "labels.icon",
+    "stylers": {
+        "visibility": "off",
+        "curZoomRegionId": "0",
+        "curZoomRegion": "13,16",
+        "level": "14"
+    }
+}, {
+    "featureType": "restaurantlabel",
+    "elementType": "labels.icon",
+    "stylers": {
+        "visibility": "off",
+        "curZoomRegionId": "0",
+        "curZoomRegion": "13,16",
+        "level": "15"
+    }
+}, {
+    "featureType": "restaurantlabel",
+    "elementType": "labels.icon",
+    "stylers": {
+        "visibility": "off",
+        "curZoomRegionId": "0",
+        "curZoomRegion": "13,16",
+        "level": "16"
+    }
+}, {
+    "featureType": "shoppinglabel",
+    "elementType": "labels.text",
+    "stylers": {
+        "fontsize": 22
+    }
+}, {
+    "featureType": "hotellabel",
+    "elementType": "labels.text",
+    "stylers": {
+        "fontsize": 22
+    }
+}, {
+    "featureType": "medicallabel",
+    "stylers": {
+        "curZoomRegionId": "0",
+        "curZoomRegion": "13,13",
+        "level": "13"
+    }
+}, {
+    "featureType": "medicallabel",
+    "elementType": "labels",
+    "stylers": {
+        "visibility": "off",
+        "curZoomRegionId": "0",
+        "curZoomRegion": "13,13",
+        "level": "13"
+    }
+}, {
+    "featureType": "entertainmentlabel",
+    "elementType": "labels",
+    "stylers": {
+        "visibility": "off"
+    }
+}, {
+    "featureType": "entertainmentlabel",
+    "elementType": "labels.icon",
+    "stylers": {
+        "visibility": "off"
+    }
+}, {
+    "featureType": "estatelabel",
+    "elementType": "labels.text",
+    "stylers": {
+        "fontsize": 22
+    }
+}, {
+    "featureType": "estatelabel",
+    "stylers": {
+        "curZoomRegionId": "0",
+        "curZoomRegion": "13,14",
+        "level": "13"
+    }
+}, {
+    "featureType": "estatelabel",
+    "stylers": {
+        "curZoomRegionId": "0",
+        "curZoomRegion": "13,14",
+        "level": "14"
+    }
+}, {
+    "featureType": "estatelabel",
+    "elementType": "labels.icon",
+    "stylers": {
+        "visibility": "off",
+        "curZoomRegionId": "0",
+        "curZoomRegion": "13,14",
+        "level": "13"
+    }
+}, {
+    "featureType": "estatelabel",
+    "elementType": "labels.icon",
+    "stylers": {
+        "visibility": "off",
+        "curZoomRegionId": "0",
+        "curZoomRegion": "13,14",
+        "level": "14"
+    }
+}, {
+    "featureType": "businesstowerlabel",
+    "stylers": {
+        "curZoomRegionId": "0",
+        "curZoomRegion": "12,14",
+        "level": "12"
+    }
+}, {
+    "featureType": "businesstowerlabel",
+    "stylers": {
+        "curZoomRegionId": "0",
+        "curZoomRegion": "12,14",
+        "level": "13"
+    }
+}, {
+    "featureType": "businesstowerlabel",
+    "stylers": {
+        "curZoomRegionId": "0",
+        "curZoomRegion": "12,14",
+        "level": "14"
+    }
+}, {
+    "featureType": "businesstowerlabel",
+    "elementType": "labels",
+    "stylers": {
+        "visibility": "off",
+        "curZoomRegionId": "0",
+        "curZoomRegion": "12,14",
+        "level": "12"
+    }
+}, {
+    "featureType": "businesstowerlabel",
+    "elementType": "labels",
+    "stylers": {
+        "visibility": "off",
+        "curZoomRegionId": "0",
+        "curZoomRegion": "12,14",
+        "level": "13"
+    }
+}, {
+    "featureType": "businesstowerlabel",
+    "elementType": "labels",
+    "stylers": {
+        "visibility": "off",
+        "curZoomRegionId": "0",
+        "curZoomRegion": "12,14",
+        "level": "14"
+    }
+}, {
+    "featureType": "businesstowerlabel",
+    "elementType": "labels.icon",
+    "stylers": {
+        "visibility": "off",
+        "curZoomRegionId": "0",
+        "curZoomRegion": "12,14",
+        "level": "12"
+    }
+}, {
+    "featureType": "businesstowerlabel",
+    "elementType": "labels.icon",
+    "stylers": {
+        "visibility": "off",
+        "curZoomRegionId": "0",
+        "curZoomRegion": "12,14",
+        "level": "13"
+    }
+}, {
+    "featureType": "businesstowerlabel",
+    "elementType": "labels.icon",
+    "stylers": {
+        "visibility": "off",
+        "curZoomRegionId": "0",
+        "curZoomRegion": "12,14",
+        "level": "14"
+    }
+}, {
+    "featureType": "companylabel",
+    "elementType": "labels",
+    "stylers": {
+        "visibility": "off"
+    }
+}, {
+    "featureType": "companylabel",
+    "elementType": "labels.icon",
+    "stylers": {
+        "visibility": "off"
+    }
+}, {
+    "featureType": "governmentlabel",
+    "elementType": "labels.icon",
+    "stylers": {
+        "visibility": "off"
+    }
+}, {
+    "featureType": "governmentlabel",
+    "stylers": {
+        "curZoomRegionId": "0",
+        "curZoomRegion": "13,14",
+        "level": "13"
+    }
+}, {
+    "featureType": "governmentlabel",
+    "stylers": {
+        "curZoomRegionId": "0",
+        "curZoomRegion": "13,14",
+        "level": "14"
+    }
+}, {
+    "featureType": "governmentlabel",
+    "elementType": "labels.text",
+    "stylers": {
+        "fontsize": 22
+    }
+}, {
+    "featureType": "shoppinglabel",
+    "stylers": {
+        "curZoomRegionId": "0",
+        "curZoomRegion": "11,13",
+        "level": "11"
+    }
+}, {
+    "featureType": "shoppinglabel",
+    "stylers": {
+        "curZoomRegionId": "0",
+        "curZoomRegion": "11,13",
+        "level": "12"
+    }
+}, {
+    "featureType": "shoppinglabel",
+    "stylers": {
+        "curZoomRegionId": "0",
+        "curZoomRegion": "11,13",
+        "level": "13"
+    }
+}, {
+    "featureType": "shoppinglabel",
+    "elementType": "labels",
+    "stylers": {
+        "visibility": "off",
+        "curZoomRegionId": "0",
+        "curZoomRegion": "11,13",
+        "level": "11"
+    }
+}, {
+    "featureType": "shoppinglabel",
+    "elementType": "labels",
+    "stylers": {
+        "visibility": "off",
+        "curZoomRegionId": "0",
+        "curZoomRegion": "11,13",
+        "level": "12"
+    }
+}, {
+    "featureType": "shoppinglabel",
+    "elementType": "labels",
+    "stylers": {
+        "visibility": "off",
+        "curZoomRegionId": "0",
+        "curZoomRegion": "11,13",
+        "level": "13"
+    }
+}, {
+    "featureType": "shoppinglabel",
+    "elementType": "labels.icon",
+    "stylers": {
+        "visibility": "off",
+        "curZoomRegionId": "0",
+        "curZoomRegion": "11,13",
+        "level": "11"
+    }
+}, {
+    "featureType": "shoppinglabel",
+    "elementType": "labels.icon",
+    "stylers": {
+        "visibility": "off",
+        "curZoomRegionId": "0",
+        "curZoomRegion": "11,13",
+        "level": "12"
+    }
+}, {
+    "featureType": "shoppinglabel",
+    "elementType": "labels.icon",
+    "stylers": {
+        "visibility": "off",
+        "curZoomRegionId": "0",
+        "curZoomRegion": "11,13",
+        "level": "13"
+    }
+}, {
+    "featureType": "carservicelabel",
+    "elementType": "labels",
+    "stylers": {
+        "visibility": "off"
+    }
+}, {
+    "featureType": "carservicelabel",
+    "elementType": "labels.icon",
+    "stylers": {
+        "visibility": "off"
+    }
+}, {
+    "featureType": "transportationlabel",
+    "elementType": "labels.text",
+    "stylers": {
+        "fontsize": 22
+    }
+}, {
+    "featureType": "financelabel",
+    "elementType": "labels.icon",
+    "stylers": {
+        "visibility": "off"
+    }
+}, {
+    "featureType": "financelabel",
+    "elementType": "labels",
+    "stylers": {
+        "visibility": "off",
+        "curZoomRegionId": "0",
+        "curZoomRegion": "15,19",
+        "level": "15"
+    }
+}, {
+    "featureType": "financelabel",
+    "elementType": "labels",
+    "stylers": {
+        "visibility": "off",
+        "curZoomRegionId": "0",
+        "curZoomRegion": "15,19",
+        "level": "16"
+    }
+}, {
+    "featureType": "financelabel",
+    "elementType": "labels",
+    "stylers": {
+        "visibility": "off",
+        "curZoomRegionId": "0",
+        "curZoomRegion": "15,19",
+        "level": "17"
+    }
+}, {
+    "featureType": "financelabel",
+    "elementType": "labels",
+    "stylers": {
+        "visibility": "off",
+        "curZoomRegionId": "0",
+        "curZoomRegion": "15,19",
+        "level": "18"
+    }
+}, {
+    "featureType": "financelabel",
+    "elementType": "labels",
+    "stylers": {
+        "visibility": "off",
+        "curZoomRegionId": "0",
+        "curZoomRegion": "15,19",
+        "level": "19"
+    }
+}, {
+    "featureType": "financelabel",
+    "elementType": "labels.icon",
+    "stylers": {
+        "visibility": "off",
+        "curZoomRegionId": "0",
+        "curZoomRegion": "15,19",
+        "level": "15"
+    }
+}, {
+    "featureType": "financelabel",
+    "elementType": "labels.icon",
+    "stylers": {
+        "visibility": "off",
+        "curZoomRegionId": "0",
+        "curZoomRegion": "15,19",
+        "level": "16"
+    }
+}, {
+    "featureType": "financelabel",
+    "elementType": "labels.icon",
+    "stylers": {
+        "visibility": "off",
+        "curZoomRegionId": "0",
+        "curZoomRegion": "15,19",
+        "level": "17"
+    }
+}, {
+    "featureType": "financelabel",
+    "elementType": "labels.icon",
+    "stylers": {
+        "visibility": "off",
+        "curZoomRegionId": "0",
+        "curZoomRegion": "15,19",
+        "level": "18"
+    }
+}, {
+    "featureType": "financelabel",
+    "elementType": "labels.icon",
+    "stylers": {
+        "visibility": "off",
+        "curZoomRegionId": "0",
+        "curZoomRegion": "15,19",
+        "level": "19"
+    }
+}, {
+    "featureType": "hotellabel",
+    "stylers": {
+        "curZoomRegionId": "0",
+        "curZoomRegion": "15,19",
+        "level": "15"
+    }
+}, {
+    "featureType": "hotellabel",
+    "stylers": {
+        "curZoomRegionId": "0",
+        "curZoomRegion": "15,19",
+        "level": "16"
+    }
+}, {
+    "featureType": "hotellabel",
+    "stylers": {
+        "curZoomRegionId": "0",
+        "curZoomRegion": "15,19",
+        "level": "17"
+    }
+}, {
+    "featureType": "hotellabel",
+    "stylers": {
+        "curZoomRegionId": "0",
+        "curZoomRegion": "15,19",
+        "level": "18"
+    }
+}, {
+    "featureType": "hotellabel",
+    "stylers": {
+        "curZoomRegionId": "0",
+        "curZoomRegion": "15,19",
+        "level": "19"
+    }
+}, {
+    "featureType": "green",
+    "elementType": "geometry",
+    "stylers": {
+        "color": "#c5ecb5ff"
+    }
+}, {
+    "featureType": "land",
+    "elementType": "geometry",
+    "stylers": {
+        "color": "#f5f5f5ff"
+    }
+}, {
+    "featureType": "scenicspots",
+    "elementType": "geometry",
+    "stylers": {
+        "color": "#c5ecb5ff"
+    }
+}, {
+    "featureType": "local",
+    "elementType": "labels.text",
+    "stylers": {
+        "fontsize": 22
+    }
+}, {
+    "featureType": "fourlevelway",
+    "elementType": "labels.text",
+    "stylers": {
+        "fontsize": 22
+    }
+}, {
+    "featureType": "estate",
+    "elementType": "geometry",
+    "stylers": {
+        "color": "#edededff"
+    }
+}];
